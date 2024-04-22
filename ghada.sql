606394ca061c6c71326c7e5d89b319a75a4325ba
kyrixdev <kefioussama595@gmail.com>
2024-04-22 20:45:01 +0100

    DataBase

---
+++ ghada.sql
@@ -0,0 +1,260 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Apr 22, 2024 at 09:44 PM
+-- Server version: 10.4.28-MariaDB
+-- PHP Version: 8.0.28
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `ghada`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `administrateur`
+--
+
+CREATE TABLE `administrateur` (
+  `id` int(11) NOT NULL,
+  `nom` varchar(50) NOT NULL,
+  `prenom` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `mp` varchar(50) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+--
+-- Dumping data for table `administrateur`
+--
+
+INSERT INTO `administrateur` (`id`, `nom`, `prenom`, `email`, `mp`) VALUES
+(2, 'admin', 'admin', 'admin@admin.admin', '21232f297a57a5a743894a0e4a801fc3');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `categories`
+--
+
+CREATE TABLE `categories` (
+  `id` int(11) NOT NULL,
+  `nom` varchar(255) NOT NULL,
+  `description` varchar(255) NOT NULL,
+  `image` varchar(255) NOT NULL,
+  `createur` int(11) NOT NULL,
+  `date_creation` date NOT NULL,
+  `date_modification` date NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+--
+-- Dumping data for table `categories`
+--
+
+INSERT INTO `categories` (`id`, `nom`, `description`, `image`, `createur`, `date_creation`, `date_modification`) VALUES
+(1, 'Bloom  ', '   Bloom categorie   ', 'blanc vert.webp', 0, '0000-00-00', '2024-04-18'),
+(16, 'Test  ', ' Test ', 'blanc vert.webp', 2, '2024-04-16', '2024-04-16'),
+(19, 'kefi', 'kefi', 'blanc vert.webp', 2, '2024-04-16', '0000-00-00'),
+(20, 'Categorie 3    a', '  Categorie 3 Description  a', 'blanc vert.webp', 2, '2024-04-18', '2024-04-18');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `commandes`
+--
+
+CREATE TABLE `commandes` (
+  `id` int(11) NOT NULL,
+  `produit` int(11) NOT NULL,
+  `quantite` int(11) NOT NULL,
+  `total` float NOT NULL,
+  `panier` int(11) NOT NULL,
+  `date_modification` date NOT NULL,
+  `date_creation` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `commandes`
+--
+
+INSERT INTO `commandes` (`id`, `produit`, `quantite`, `total`, `panier`, `date_modification`, `date_creation`) VALUES
+(8, 1, 2, 2462, 2, '0000-00-00', '2024-04-19'),
+(9, 4, 3, 21000, 3, '0000-00-00', '2024-04-19'),
+(10, 1, 1, 1231, 4, '0000-00-00', '2024-04-21'),
+(11, 1, 1, 1231, 5, '0000-00-00', '2024-04-21'),
+(12, 3, 2, 16000, 6, '0000-00-00', '2024-04-22'),
+(13, 4, 1, 7000, 6, '0000-00-00', '2024-04-22'),
+(14, 2, 1, 141, 7, '0000-00-00', '2024-04-22'),
+(15, 2, 1, 141, 8, '0000-00-00', '2024-04-22');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `panier`
+--
+
+CREATE TABLE `panier` (
+  `id` int(11) NOT NULL,
+  `visiteur` int(11) NOT NULL,
+  `total` float DEFAULT NULL,
+  `etat` varchar(50) NOT NULL DEFAULT 'En cours',
+  `date_creation` date NOT NULL,
+  `date_modificatiion` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `panier`
+--
+
+INSERT INTO `panier` (`id`, `visiteur`, `total`, `etat`, `date_creation`, `date_modificatiion`) VALUES
+(7, 15, 141, 'En cours', '2024-04-22', '0000-00-00'),
+(8, 15, 141, 'En cours', '2024-04-22', '0000-00-00');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `produit`
+--
+
+CREATE TABLE `produit` (
+  `id` int(11) NOT NULL,
+  `nom` varchar(255) NOT NULL,
+  `prix` float NOT NULL,
+  `description` text NOT NULL,
+  `image` text NOT NULL,
+  `categorie` int(11) NOT NULL,
+  `stock` int(11) NOT NULL,
+  `date_creation` date NOT NULL,
+  `date_modification` date NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+--
+-- Dumping data for table `produit`
+--
+
+INSERT INTO `produit` (`id`, `nom`, `prix`, `description`, `image`, `categorie`, `stock`, `date_creation`, `date_modification`) VALUES
+(1, 'produit1                  ', 1231, '         description produit 1   a      ', 'blanc vert.webp', 1, 0, '0000-00-00', '2024-04-18'),
+(2, 'produit 2          ', 141, '     description produit 2     ', 'blanc vert.webp', 1, 12, '0000-00-00', '2024-04-21'),
+(3, 'produit3      ', 8000, '   description3   ', 'blanc vert.webp', 1, 0, '0000-00-00', '2024-04-18'),
+(4, 'produit4', 7000, 'description produit 4', 'blanc vert.webp', 1, 0, '0000-00-00', '0000-00-00');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `visiteurs`
+--
+
+CREATE TABLE `visiteurs` (
+  `id` int(11) NOT NULL,
+  `nom` varchar(255) NOT NULL,
+  `prenom` varchar(255) NOT NULL,
+  `mp` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `telephone` varchar(12) NOT NULL,
+  `etat` int(11) DEFAULT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+--
+-- Dumping data for table `visiteurs`
+--
+
+INSERT INTO `visiteurs` (`id`, `nom`, `prenom`, `mp`, `email`, `telephone`, `etat`) VALUES
+(17, 'tex', 'te', 'd41d8cd98f00b204e9800998ecf8427e', 'kefioussama@gmail.com', '123', NULL),
+(16, 'tex', 'te', 'd41d8cd98f00b204e9800998ecf8427e', 'kefioussama@gmail.com', '123', NULL),
+(15, 'Kefi', 'Oussama', '7d74f6896e07adce917c12a416944b0e', 'kefioussama85@gmail.com', '20222', 1);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `administrateur`
+--
+ALTER TABLE `administrateur`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `categories`
+--
+ALTER TABLE `categories`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `commandes`
+--
+ALTER TABLE `commandes`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `panier`
+--
+ALTER TABLE `panier`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `produit`
+--
+ALTER TABLE `produit`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `visiteurs`
+--
+ALTER TABLE `visiteurs`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `administrateur`
+--
+ALTER TABLE `administrateur`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `categories`
+--
+ALTER TABLE `categories`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
+
+--
+-- AUTO_INCREMENT for table `commandes`
+--
+ALTER TABLE `commandes`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+
+--
+-- AUTO_INCREMENT for table `panier`
+--
+ALTER TABLE `panier`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
+--
+-- AUTO_INCREMENT for table `produit`
+--
+ALTER TABLE `produit`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+
+--
+-- AUTO_INCREMENT for table `visiteurs`
+--
+ALTER TABLE `visiteurs`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
